518e6767bc69f3d9c05b835ca860605c385a1efa
Fabio de Stefani <fabiostefani@gmail.com>
2021-04-30 14:11:11 -0300

    Serviço de aplicação + AutoMapper

---
+++ src/Services/Catalogo/NerdStore.Catalogo.Application/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,20 @@
+using AutoMapper;
+using NerdStore.Catalogo.Application.ViewModels;
+using NerdStore.Catalogo.Domain.Categorias;
+using NerdStore.Catalogo.Domain.Produtos;
+
+namespace NerdStore.Catalogo.Application.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public DomainToViewModelMappingProfile()
+        {
+            CreateMap<Categoria, CategoriaViewModel>();
+            
+            CreateMap<Produto, ProdutoViewModel>()
+                .ForMember(d => d.Largura, o => o.MapFrom(s => s.Dimensoes.Largura))
+                .ForMember(d => d.Altura, o => o.MapFrom(s => s.Dimensoes.Altura))
+                .ForMember(d => d.Profundidade, o => o.MapFrom(s => s.Dimensoes.Profundidade));
+        }
+    }
+}
---
+++ src/Services/Catalogo/NerdStore.Catalogo.Application/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,20 @@
+using AutoMapper;
+using NerdStore.Catalogo.Application.ViewModels;
+using NerdStore.Catalogo.Domain.Categorias;
+using NerdStore.Catalogo.Domain.DomainObjects;
+using NerdStore.Catalogo.Domain.Produtos;
+
+namespace NerdStore.Catalogo.Application.AutoMapper
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        public ViewModelToDomainMappingProfile()
+        {
+            CreateMap<CategoriaViewModel, Categoria>()
+            .ConstructUsing(c => new Categoria(c.Nome, c.Codigo));
+
+            CreateMap<ProdutoViewModel, Produto>()
+            .ConstructUsing(p=>new Produto(p.Nome, p.Descricao, p.Ativo, p.Valor, p.CategoriaId, p.DataCadastro, p.Imagem, new Dimensoes(p.Altura, p.Largura, p.Profundidade  )));
+        }
+    }
+}
